9d16c1836751acbdd26c1c55e2e2e60b74fb4fe7
Yojayro <yojayro.ito@upeu.edu.pe>
2019-08-23 11:02:00 -0500

    Segundo Cambio

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,33 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { products } from '../products';
 
 @Component({
-  selector: 'content',
+  selector: 'app-root',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-  products =  [
-    {
-      name: 'Phone XL',
-      price: 799,
-      description: 'A large phone with one of the best screens'
-    },
-    {
-      name: 'Phone Mini',
-      price: 699,
-      description: 'A great phone with one of the best cameras'
-    },
-    {
-      name: 'Phone Standard',
-      price: 299,
-      description: ''
-    }
-  ];
-  ;
+  products=products;
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  share(id){
+    console.log("id="+id);
+  }
 }
